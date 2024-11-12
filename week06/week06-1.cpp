98430927f61a48bc94f1998f89db83195abed6a1
yellowmars <145088366+yellowmars@users.noreply.github.com>
2024-11-13 07:59:31 +0800

    week06

---
+++ week06/week06-1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main()
+{
+    vector<int> a;///伸縮自如，可變長度的陣列
+    a.push_back(30);
+    a.push_back(20);
+    a.push_back(10);
+    for(int i=0; i<3; i++){
+        cout<<a[i]<<"\n";
+    }
+    sort(a.begin(),a.end());
+    for(int i=0; i<3; i++){
+        cout<<a[i]<<" ";
+    }
+
+}
---
+++ week06/week06-2a.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    vector<int> a;
+    int now;
+    while(cin>>now){
+        a.push_back(now);
+        cout<<now<<"\n";
+    }
+
+}
